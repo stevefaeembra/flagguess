70e3c6cf43c5bf390e48d0ba2a2f65bd5e9fe047
stevefaeembra <stevendkay@gmail.com>
2023-01-28 21:31:27 +0000

    start adding hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import { FlagCard } from "./components/flagcard";
 import { getRandomFlag } from "./utilities/flagdata";
 
 function App() {
-  const { isocode, name } = getRandomFlag();
+  const [isoCode, setIsoCode] = useState("");
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    const { isocode, name } = getRandomFlag();
+    setIsoCode(isocode);
+    setName(name);
+    console.log(`Current flag ${isocode} ${name}`);
+  }, [setIsoCode, setName, getRandomFlag]);
+
   return (
     <>
       <div>
         <h1>Guess the flag</h1>
         <br />
       </div>
-      <FlagCard isoCode={isocode} name={name} />
+      <FlagCard isoCode={isoCode} name={name} />
     </>
   );
 }
