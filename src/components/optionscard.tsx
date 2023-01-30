72acd9f2f51bd0f7d3bd0797949da96a7a07e252
stevefaeembra <stevendkay@gmail.com>
2023-01-30 21:19:36 +0000

    fix key error

--- src/components/optionscard.tsx
+++ src/components/optionscard.tsx
@@ -8,7 +8,9 @@ export function OptionsCard({ ...props }: Props) {
   return (
     <div className="grid grid-cols-2 gap-1">
       {props?.choices?.map((item) => (
-        <button className="btn">{item.name}</button>
+        <button key={`btn_${item.isocode}`} className="btn">
+          {item.name}
+        </button>
       ))}
     </div>
   );
