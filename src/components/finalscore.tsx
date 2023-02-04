e52b0bce84f80edc0bf2e3914c828f7c140863ed
stevefaeembra <stevendkay@gmail.com>
2023-02-04 19:40:33 +0000

    improve result card

--- src/components/finalscore.tsx
+++ src/components/finalscore.tsx
@@ -5,7 +5,7 @@ type Props = {
 };
 
 export function FinalScore({ ...props }: Props) {
-  const percent = parseInt((props.numberRight / props.numberRounds) * 100);
+  const percent = ((props.numberRight / props.numberRounds) * 100).toFixed(0);
   return (
     <>
       <div className="mt-6">
@@ -15,7 +15,11 @@ export function FinalScore({ ...props }: Props) {
         <h1>
           {props.numberRight} / {props.numberRounds}
         </h1>
-        <h1>{percent} %</h1>
+        <div>
+          <div className="radial-progress text-primary" style={{ "--value": percent }}>
+            {percent}%
+          </div>
+        </div>
       </div>
       <div className="mt-6">
         <button onClick={() => props.onNewGameClick()} className="btn btn-block">
