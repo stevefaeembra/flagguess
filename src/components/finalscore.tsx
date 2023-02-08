035bcc6ee6f761e3151126daace4f81a5980a472
stevefaeembra <stevendkay@gmail.com>
2023-02-08 19:27:53 +0000

    pass judgement

--- src/components/finalscore.tsx
+++ src/components/finalscore.tsx
@@ -5,7 +5,17 @@ type Props = {
 };
 
 export function FinalScore({ ...props }: Props) {
-  const percent = ((props.numberRight / props.numberRounds) * 100).toFixed(0);
+  const percent = parseInt(((props.numberRight / props.numberRounds) * 100).toFixed(0));
+  let result = "";
+  percent === 100
+    ? (result = "Perfection!")
+    : percent > 90
+    ? (result = "Excellent!")
+    : percent > 80
+    ? (result = "Superb!")
+    : percent > 60
+    ? (result = "Great!")
+    : (result = "Good Game!");
   return (
     <>
       <div className="mt-6">
@@ -16,6 +26,7 @@ export function FinalScore({ ...props }: Props) {
           {props.numberRight} / {props.numberRounds}
         </h1>
         <h1 className="text-red-900">{percent}%</h1>
+        <h1>{result}</h1>
       </div>
       <div className="mt-6">
         <button onClick={() => props.onNewGameClick()} className="btn btn-block">
